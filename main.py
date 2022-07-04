6f03e1a18d5015d83ee91c3af57b3fc1fc685bcd
anasce <ana.scepanovic57@gmail.com>
2022-07-04 09:28:13 +0200

    master main 4

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ def add_new_post():
             subtitle=form.subtitle.data,
             body=form.body.data,
             img_url=form.img_url.data,
-            #author=current_user.name,
+
 
             date=date.today().strftime("%B %d, %Y"),
             reguser_id=sikor.id
