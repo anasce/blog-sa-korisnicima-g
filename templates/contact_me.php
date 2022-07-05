df2e81c1021921453e45f59fb7b672c2aeae7792
anasce <ana.scepanovic57@gmail.com>
2022-07-05 11:16:16 +0200

    kontakt forma 12

--- templates/contact_me.php
+++ templates/contact_me.php
@@ -1,11 +1,8 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message   - $phone";
-$recipient = "ana.scepanovic57@google.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php
+
+$files = scandir(".", 1);
+echo "<pre>";
+print_r($files);
+echo "</pre>";
+
 ?>
